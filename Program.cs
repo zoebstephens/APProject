6891449b3aeaf2c032dc3dfddddb317c84e5fac0
zoebstephens <zoebstephens@gmail.com>
2022-03-24 12:18:04 -0700

    ap project

--- Program.cs
+++ Program.cs
@@ -7,62 +7,120 @@ namespace APProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
 
         }
 
-        public BreakfastGame(int minimum, int maximum)
-        {
-            this.minimum = minimum;
-            this.maximum = maximum;
-        }
 
-        public static void Play(int minimum, int maximum)
+
+        public static List<BreakfastItem> GetBreakfast()
         {
+            List<BreakfastItem> BreakfastCombo = new List<BreakfastItem>();
+            BreakfastItem bananaBread = new BreakfastItem();
+            bananaBread.Description = "Delicious Banana Bread";
+            bananaBread.price = 4;
+            
+            BreakfastItem waffle = new BreakfastItem();
+            waffle.Description = "Delicious Waffle";
+            waffle.price = 8;
+
+            BreakfastItem cereal = new BreakfastItem();
+            cereal.Description = "Delicious Cereal";
+            cereal.price = 2;
+
+            BreakfastItem pancake = new BreakfastItem();
+            pancake.Description = "Delicious Pancake";
+            pancake.price = 7;
+
+            BreakfastItem bagel = new BreakfastItem();
+            bagel.Description = "Delicious Bagel";
+            bagel.price = 1;
+
+            BreakfastItem croissant = new BreakfastItem();
+            croissant.Description = "Delicious Croissant";
+            croissant.price = 5;
+
+            BreakfastItem oatmeal = new BreakfastItem();
+            oatmeal.Description = "Delicious Oatmeal";
+            oatmeal.price = 2;
+
+            BreakfastItem eggs = new BreakfastItem();
+            eggs.Description = "Delicious Eggs";
+            eggs.price = 3;
+
+            BreakfastItem crepe = new BreakfastItem();
+            crepe.Description = "Delicious Crepe";
+            crepe.price = 10;
+
+            BreakfastItem blueberryMuffin = new BreakfastItem();
+            blueberryMuffin.Description = "Delicious Blueberry Muffin";
+            blueberryMuffin.price = 6;
+
+            BreakfastCombo.Add(bananaBread);
+            BreakfastCombo.Add(waffle);
+            BreakfastCombo.Add(cereal);
+            BreakfastCombo.Add(pancake);
+            BreakfastCombo.Add(bagel);
+            BreakfastCombo.Add(croissant);
+            BreakfastCombo.Add(oatmeal);
+            BreakfastCombo.Add(eggs);
+            BreakfastCombo.Add(crepe);
+            BreakfastCombo.Add(blueberryMuffin);
     
-            List<int> guesses; // A list of all the players inputs
 
-            inputs.Add(playerInput);
-
-            while (playerInput < 3)
+            foreach(BreakfastItem item in BreakfastCombo)
             {
-                Console.WriteLine(CheckGuess(playerGuess, randomNumber));
-                playerGuess = GetGuess(minimum, maximum);
-                guesses.Add(playerGuess);
+                Console.WriteLine($"{item.Description}");
             }
 
-            DisplayReport(guesses);
+            return BreakfastCombo;
+
         }
 
-
-        public int GetBreakfast()
+        public static int GetDescription(BreakfastItem description)
         {
-            int input;
-            Console.WriteLine($"Enter a number between {this.minimum} and {this.maximum}:");
-            input= int.Parse(Console.ReadLine());
-            if (input < this.minimum || input > this.maximum)
+            if (description == null)
             {
-                Console.Error.WriteLine("Invalid input!");
-                return this.GetBreakfast();
+                throw new ArgumentNullException("Description can not be null");
+
             }
 
-            return input;
+            List<string> items = description.items;
+
+            if (items.Count == 0)
+            {
+                throw new ArgumentException("The list must contain at least 1 option");
+            }
+
+            Console.WriteLine(description.Description);
+
+            int ix = 1;
+            foreach (string item in items)
+            {
+                Console.WriteLine($"{ix}. {items}");
+                ix = ix + 1;
+            }
+
+            // Display Description (List of Options
+            // Loop through each answer and display it 
+            // Use GetUserInput method to get the user's response
+            return GetUserInput(description.items);
         }
 
-        public static int GetValidAnswer(List<string> answers)
+        public static int GetUserInput(List<string> items)
         {
-            // Validate that there is at least 1 possible answer.
-            // If the list of answers is empty, throw an exception
+            // Validate that there is at least 1 possible item.
+            // If the list of items is empty, throw an exception
             // Otherwise, Display a message asking the user to select an option.
             // Store the user's response in a variable name choice
             // Validate that choice is one of the possible answers
-            // If it is not a valid answer
+            // If it is not a valid item
             // Display an error message
             // Go to 3
             // Otherwise, return the user's choice.
-            if (answers.Count == 0)
+            if (items.Count == 0)
             {
-                throw new Exception($"At least one possible answer does not exist.");
+                throw new Exception($"At least one possible item does not exist.");
             }
 
             Console.WriteLine("Select an Option");
@@ -70,7 +128,7 @@ namespace APProject
 
             do
             {
-                Console.Write($"Enter a number between 1 and {answers.Count}: ");
+                Console.Write($"Enter a number between 1 and {items.Count}: ");
                 string input = Console.ReadLine();
 
                 bool isANumber = int.TryParse(input, out userChoice);
@@ -83,74 +141,23 @@ namespace APProject
                 {
                     Console.Error.WriteLine("That number is not greater than 1.");
                 }
-                else if (userChoice > answers.Count)
+                else if (userChoice > items.Count)
                 {
-                    Console.Error.WriteLine($"That number is not less than {answers.Count}.");
+                    Console.Error.WriteLine($"That number is not less than {items.Count}.");
                 }
             }
-            while (userChoice < 1 || userChoice > answers.Count);
+            while (userChoice < 1 || userChoice > items.Count);
 
-            return userChoice -1;
-        }
-
-
-        public string CheckInput (int input)
-        {
-            if (input > this.comboMealNumber)
-            {
-                return "Your number of selected breakfast options is too low.";
-            }
-            else
-            {
-                return "Your number of selected breakfast options is too high.";
-            }
-        }
-
-    
-        public void DisplayReport()
-        {
-            Console.WriteLine("You selected the 3 piece combo breakfast!");
-            if (this.inputs.Count > 1)
-            {
-                Console.WriteLine($"You didn't select the correct number of items {this.inputs.Count - 1} times.");
-                Console.Write("Inputs:");
-                foreach (int input in this.inputs)
-                {
-                    Console.Write($"{input}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("You selected the correct number of items on the first try! Yay you!");
-            }
-
-        
-        public static string GetBreakfast()
-        {
-            List<string> words = new List<string>();
-            words.Add("bagel");
-            words.Add("ego waffle");
-            words.Add("cereal");
-            words.Add("sausage");
-            words.Add("pancake");
-            words.Add("avocado toast");
-            words.Add("oatmeal");
-            words.Add("acai bowl");
-            words.Add("eggs");
-            words.Add("yogurt");
-            words.Add("breakfast burrito");
-            words.Add("fruit platter");
-            words.Add("crepe");
-            words.Add("croissant");
-            words.Add("blueberry muffin");
-            words.Add("chocolate chip pancake");
-            words.Add("banana nut muffin");
-            words.Add("french toast");
-            words.Add("toast and jam");
-            words.Add("smoothie");
-
-
-            return Breakfast;
+            return userChoice - 1;
         }
     }
 }
+
+public class BreakfastItem
+{
+    public string Description;
+    public decimal price;
+
+    public List<string> items = new List<string>();
+}
+
